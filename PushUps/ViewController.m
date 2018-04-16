0e7d0ef8b0ed33391ce13b7910db3fdc25e60033
Devepre <bicycle2life@gmail.com>
2018-04-16 17:41:02 +0300

    Proximity sensor code added

--- PushUps/ViewController.m
+++ PushUps/ViewController.m
@@ -1,3 +1,4 @@
+#import <AVFoundation/AVFoundation.h>
 #import "ViewController.h"
 
 @interface ViewController ()
@@ -6,16 +7,40 @@
 
 @implementation ViewController
 
+- (void)dealloc {
+    [[NSNotificationCenter defaultCenter] removeObserver:self
+                                                    name:UIDeviceProximityStateDidChangeNotification
+                                                  object:nil];
+    
+    [[UIApplication sharedApplication] setIdleTimerDisabled:NO];
+}
+
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    [[UIDevice currentDevice] setProximityMonitoringEnabled:YES];
+    
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(sensorStateChange:)
+                                                 name:UIDeviceProximityStateDidChangeNotification
+                                               object:nil];
 }
 
+#pragma mark - Additional Methods
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)sensorStateChange:(NSNotificationCenter *)notification {
+    if ([UIDevice.currentDevice proximityState] == YES) {
+        NSLog(@"Close");
+        
+        AVCaptureDevice *device = [AVCaptureDevice defaultDeviceWithMediaType:AVMediaTypeVideo];
+        if (device.hasTorch) {
+            [device lockForConfiguration:nil];
+            device.torchMode = device.torchMode ? AVCaptureTorchModeOff : AVCaptureTorchModeOn;
+            [device unlockForConfiguration];
+        }
+    } else {
+        NSLog(@"Not close");
+    }
 }
-
 
 @end
