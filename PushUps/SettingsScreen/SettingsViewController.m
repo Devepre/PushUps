0a8e73932b174631fa301d0818ba4f6469b69730
Devepre <bicycle2life@gmail.com>
2018-04-18 18:20:53 +0300

    Full Session DB creating

--- PushUps/SettingsScreen/SettingsViewController.m
+++ PushUps/SettingsScreen/SettingsViewController.m
@@ -47,15 +47,16 @@
     NSMutableArray *sessionsArray = [[NSMutableArray alloc] init];
     for (int i = 0; i < sessionMaxValues.count; i++) {
         SessionMO *currentSession = [self createSessionWithTitle:[NSString stringWithFormat:@"%@ - %@ push-ups",
-                                                                   [sessionMinValues objectAtIndex:i],
-                                                                   [sessionMaxValues objectAtIndex:i]]
-                                                      andMinValue:0
-                                                      andMaxValue:5
-                                                            andId:0];
+                                                                  [sessionMinValues objectAtIndex:i],
+                                                                  [sessionMaxValues objectAtIndex:i]]
+                                                     andMinValue:[[sessionMinValues objectAtIndex:i] intValue]
+                                                     andMaxValue:[[sessionMaxValues objectAtIndex:i] intValue]
+                                                           andId:i];
         [sessionsArray addObject:currentSession];
     }
     
-    SessionMO *session0 = [sessionsArray objectAtIndex:0];
+    int index = 0;
+    // Session 1
     NSArray *setCounts1 = @[@2, @3, @2, @2, @3];
     NSArray *setCounts2 = @[@3, @4, @2, @3, @4];
     NSArray *setCounts3 = @[@4, @5, @4, @4, @5];
@@ -65,10 +66,122 @@
     NSArray *days = @[setCounts1, setCounts2, setCounts3, setCounts4, setCounts5, setCounts6];
     NSArray *dayRelaxIntervalSeconds = @[@60, @90, @120, @60, @90, @120];
     NSArray *dayBreakIntervalDays = @[@1, @1, @2, @1, @1, @2];
+    [[sessionsArray objectAtIndex:index++] setDaysArray: [self createDaysFromDaysArray:days
+                                                withDayRelaxIntervalSecondsArray:dayRelaxIntervalSeconds
+                                                    andDayBreakIntervalDaysArray:dayBreakIntervalDays]];
     
-    session0.daysArray = [self createDaysFromDaysArray:days
-                        withDayRelaxIntervalSecondsArray:dayRelaxIntervalSeconds
-                            andDayBreakIntervalDaysArray:dayBreakIntervalDays];
+    // Session 2
+    setCounts1 = @[@5,  @6,  @4,  @4,  @5];
+    setCounts2 = @[@6,  @7,  @6,  @6,  @7];
+    setCounts3 = @[@8,  @10, @7,  @7,  @10];
+    setCounts4 = @[@9,  @11, @8,  @8,  @11];
+    setCounts5 = @[@10, @12, @9,  @9,  @13];
+    setCounts6 = @[@12, @13, @10, @10, @15];
+    days = @[setCounts1, setCounts2, setCounts3, setCounts4, setCounts5, setCounts6];
+    [[sessionsArray objectAtIndex:index++] setDaysArray: [self createDaysFromDaysArray:days
+                                                withDayRelaxIntervalSecondsArray:dayRelaxIntervalSeconds
+                                                    andDayBreakIntervalDaysArray:dayBreakIntervalDays]];
+    
+    // Session 3
+    setCounts1 = @[@8,  @9,  @7,  @7,  @8];
+    setCounts2 = @[@9,  @10, @8,  @8,  @10];
+    setCounts3 = @[@11, @13, @9,  @9,  @13];
+    setCounts4 = @[@12, @14, @10, @10, @15];
+    setCounts5 = @[@13, @15, @11, @11, @17];
+    setCounts6 = @[@14, @16, @13, @13, @19];
+    days = @[setCounts1, setCounts2, setCounts3, setCounts4, setCounts5, setCounts6];
+    [[sessionsArray objectAtIndex:index++] setDaysArray: [self createDaysFromDaysArray:days
+                                                withDayRelaxIntervalSecondsArray:dayRelaxIntervalSeconds
+                                                    andDayBreakIntervalDaysArray:dayBreakIntervalDays]];
+    
+    // Session 4
+    setCounts1 = @[@12, @17, @13, @13, @17];
+    setCounts2 = @[@14, @19, @14, @14, @19];
+    setCounts3 = @[@16, @21, @15, @15, @21];
+    setCounts4 = @[@18, @22, @16, @16, @21];
+    setCounts5 = @[@20, @25, @20, @20, @23];
+    setCounts6 = @[@23, @28, @22, @22, @25];
+    days = @[setCounts1, setCounts2, setCounts3, setCounts4, setCounts5, setCounts6];
+    [[sessionsArray objectAtIndex:index++] setDaysArray: [self createDaysFromDaysArray:days
+                                                withDayRelaxIntervalSecondsArray:dayRelaxIntervalSeconds
+                                                    andDayBreakIntervalDaysArray:dayBreakIntervalDays]];
+    
+    // Session 5
+    setCounts1 = @[@14, @18, @14, @14, @20];
+    setCounts2 = @[@20, @25, @15, @15, @23];
+    setCounts3 = @[@20, @27, @18, @18, @25];
+    setCounts4 = @[@21, @25, @21, @21, @27];
+    setCounts5 = @[@25, @29, @25, @25, @30];
+    setCounts6 = @[@29, @33, @29, @29, @33];
+    days = @[setCounts1, setCounts2, setCounts3, setCounts4, setCounts5, setCounts6];
+    [[sessionsArray objectAtIndex:index++] setDaysArray: [self createDaysFromDaysArray:days
+                                                withDayRelaxIntervalSecondsArray:dayRelaxIntervalSeconds
+                                                    andDayBreakIntervalDaysArray:dayBreakIntervalDays]];
+    
+    // Session 6
+    setCounts1 = @[@17, @19, @15, @15, @20];
+    setCounts2 = @[@10, @10, @13, @13, @10, @10, @9,  @25];
+    setCounts3 = @[@13, @13, @15, @15, @12, @12, @10, @30];
+    days = @[setCounts1, setCounts2, setCounts3];
+    dayRelaxIntervalSeconds = @[@60, @45, @45];
+    dayBreakIntervalDays = @[@1, @1, @2];
+    [[sessionsArray objectAtIndex:index++] setDaysArray: [self createDaysFromDaysArray:days
+                                                withDayRelaxIntervalSecondsArray:dayRelaxIntervalSeconds
+                                                    andDayBreakIntervalDaysArray:dayBreakIntervalDays]];
+    
+    // Session 7
+    setCounts1 = @[@22, @24, @20, @20, @25];
+    setCounts2 = @[@15, @15, @18, @18, @15, @15, @14, @30];
+    setCounts3 = @[@18, @18, @20, @20, @17, @17, @15, @35];
+    days = @[setCounts1, setCounts2, setCounts3];
+    [[sessionsArray objectAtIndex:index++] setDaysArray: [self createDaysFromDaysArray:days
+                                                      withDayRelaxIntervalSecondsArray:dayRelaxIntervalSeconds
+                                                          andDayBreakIntervalDaysArray:dayBreakIntervalDays]];
+    
+    // Session 8
+    setCounts1 = @[@27, @29, @25, @25, @35];
+    setCounts2 = @[@19, @19, @22, @22, @18, @18, @22, @35];
+    setCounts3 = @[@20, @20, @24, @24, @20, @20, @22, @40];
+    days = @[setCounts1, setCounts2, setCounts3];
+    [[sessionsArray objectAtIndex:index++] setDaysArray: [self createDaysFromDaysArray:days
+                                                      withDayRelaxIntervalSecondsArray:dayRelaxIntervalSeconds
+                                                          andDayBreakIntervalDaysArray:dayBreakIntervalDays]];
+    
+    // Session 9
+    setCounts1 = @[@30, @34, @30, @30, @40];
+    setCounts2 = @[@19, @19, @23, @23, @19, @19, @22, @37];
+    setCounts3 = @[@20, @20, @27, @27, @21, @21, @21, @44];
+    days = @[setCounts1, setCounts2, setCounts3];
+    [[sessionsArray objectAtIndex:index++] setDaysArray: [self createDaysFromDaysArray:days
+                                                      withDayRelaxIntervalSecondsArray:dayRelaxIntervalSeconds
+                                                          andDayBreakIntervalDaysArray:dayBreakIntervalDays]];
+    
+    // Session 10
+    setCounts1 = @[@30, @39, @35, @35, @42];
+    setCounts2 = @[@20, @20, @23, @23, @20, @20, @18, @18, @53];
+    setCounts3 = @[@22, @22, @30, @30, @25, @25, @18, @18, @55];
+    days = @[setCounts1, setCounts2, setCounts3];
+    [[sessionsArray objectAtIndex:index++] setDaysArray: [self createDaysFromDaysArray:days
+                                                      withDayRelaxIntervalSecondsArray:dayRelaxIntervalSeconds
+                                                          andDayBreakIntervalDaysArray:dayBreakIntervalDays]];
+    
+    // Session 11
+    setCounts1 = @[@30, @44, @40, @40, @55];
+    setCounts2 = @[@22, @22, @27, @27, @24, @23, @18, @18, @58];
+    setCounts3 = @[@26, @26, @33, @33, @26, @26, @22, @22, @60];
+    days = @[setCounts1, setCounts2, setCounts3];
+    [[sessionsArray objectAtIndex:index++] setDaysArray: [self createDaysFromDaysArray:days
+                                                      withDayRelaxIntervalSecondsArray:dayRelaxIntervalSeconds
+                                                          andDayBreakIntervalDaysArray:dayBreakIntervalDays]];
+    
+    // Session 12
+    setCounts1 = @[@35, @49, @45, @45, @55];
+    setCounts2 = @[@22, @22, @30, @30, @24, @24, @18, @18, @59];
+    setCounts3 = @[@28, @28, @35, @35, @27, @27, @23, @23, @60];
+    days = @[setCounts1, setCounts2, setCounts3];
+    [[sessionsArray objectAtIndex:index++] setDaysArray: [self createDaysFromDaysArray:days
+                                                      withDayRelaxIntervalSecondsArray:dayRelaxIntervalSeconds
+                                                          andDayBreakIntervalDaysArray:dayBreakIntervalDays]];
     
     [self saveManagedObjecContext];
 }
